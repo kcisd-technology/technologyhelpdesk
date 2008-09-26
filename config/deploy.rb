cfcb42b4ed0aa197c1b4fdd933b149b12128f1d5
Wade West <eod_81@yahoo.com>
2008-09-26 14:31:59 -0500

    Changed the home page a little

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,29 @@ set :repository,  "git@technology:helpdesk.git"
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/rails/#{application}"
+set :user, 'wadewest'
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
+set :branch, 'master'
 
 role :app, "technology"
 role :web, "technology"
 role :db,  "technology", :primary => true
+
+namespace :deploy do
+  desc "Restart Application" 
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+  desc "Start Application -- not needed for Passenger" 
+  task :start, :roles => :app do
+    # nothing -- need to override default cap start task when using Passenger
+  end
+end
+
+task :link_database_config do
+  run "ln -s #{deploy_to}/shared/system/database.yml #{deploy_to}/current/config/database.yml"
+end
+before "deploy:migrate", :link_database_config
