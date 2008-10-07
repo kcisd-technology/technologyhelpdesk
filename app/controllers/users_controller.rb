551b5d0482a046149c55206ac8c849cb073ab949
Wade West <eod_81@yahoo.com>
2008-10-07 10:17:34 -0500

    Added some user management features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   
   before_filter :login_required
-  access_control [:index, :show, :new, :create] => 'admin' 
+  access_control [:index, :show, :new, :create, :edit, :update] => 'admin' 
 
   def index
     @users = User.all
   end
   
   def show
-    redirect_to users_url
+    #redirect_to users_url
+    @user = User.find(params[:id])
   end
 
   # render new.rhtml
@@ -24,11 +25,30 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     @user.save
     if @user.errors.empty?
-      self.current_user = @user
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!"
+      #self.current_user = @user
+      redirect_back_or_default(@user)
+      flash[:notice] = "User was created"
     else
       render :action => 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        @user.role_ids = params[:roles]
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(@user) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @usererrors, :status => :unprocessable_entity }
+      end
     end
   end
   
