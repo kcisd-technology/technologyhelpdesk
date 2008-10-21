72e5e04bacb3f528586180bbfd563126f1f5218c
wadewest <eod_81@yahoo.com>
2008-10-21 11:34:23 -0700

    Changed repo location

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "Technology Helpdesk"
-set :repository,  "git@technology:helpdesk.git"
+set :repository,  "git@github.com:wadewest/technolgoyhelpdesk.git"
 set :scm, :git
 set :branch, 'master'
 set :user, 'wadewest'
