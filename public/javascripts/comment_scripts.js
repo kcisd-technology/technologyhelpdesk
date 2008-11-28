571dd866842af132aef0e7357cae9f2b184992b4
Wade West <eod_81@yahoo.com>
2008-11-28 03:55:00 -0600

    Moved the styles for comments and added some javascript

--- public/javascripts/comment_scripts.js
+++ public/javascripts/comment_scripts.js
@@ -4,7 +4,7 @@ var loadCommentControls = function() {
     var controls = comment.getElementsByClassName('controls')[0];
     controls.insert({top: ' | '});
     
-    var toggleLink = new Element('a', { href:"#"});
+    var toggleLink = new Element('a', { href:"#", 'class':'showToggle'});
     toggleLink.insert("Hide Comment");
     toggleLink.observe('click', toggleComment)
     controls.insert({top: toggleLink});    
@@ -13,12 +13,14 @@ var loadCommentControls = function() {
 var toggleComment = function(e) {
   var commentBody = this.up(".comment").down(".body");
   if(commentBody.visible()){
-      commentBody.hide();
+      Effect.BlindUp(commentBody);
+      //commentBody.hide();
       this.update("Show Comment");
   }else{
-      commentBody.show();
+      Effect.BlindDown(commentBody);
+      //commentBody.show();
       this.update("Hide Comment");
   }
   e.stop();
 }
-Event.observe(window, 'dom:loaded', loadCommentControls)
+Event.observe(window, 'load', loadCommentControls)
