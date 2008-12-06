ffdd177e323deffb95183eedc5106ce7a5bc4964
Wade West <eod_81@yahoo.com>
2008-12-06 02:00:29 -0600

    Created THD namespace for this apps javascripts

--- public/javascripts/comment_scripts.js
+++ public/javascripts/comment_scripts.js
@@ -1,82 +1,100 @@
-var loadCommentControls = function() {
-  var comments = document.getElementsByClassName('comment');
-  $A(comments).each( function(comment){
-    var controls = comment.getElementsByClassName('controls')[0];
-    controls.insert({top: ' | '});
+if (typeof THD == 'undefined') {
+  THD = {};
+}
+
+Object.extend( THD, {
+  
+  busyImage : function(){
+    var img = new Image();
+    img.src = THD.RootPath+"/images/busy.gif";
+    return img;
+  }(),
+  
+  Comment : {
+    loadCommentControls : function() {
+      var comments = document.getElementsByClassName('comment');
+      $A(comments).each( function(comment){
+        var controls = comment.getElementsByClassName('controls')[0];
+        controls.insert({top: ' | '});
+
+        var toggleLink = new Element('a', { href:"#", 'class':'showToggle'});
+        toggleLink.insert("Hide Comment");
+        toggleLink.observe('click', THD.Comment.toggleComment)
+        controls.insert({top: toggleLink});
+
+        var editLink = comment.getElementsByClassName('edit-link')[0];
+        editLink.observe('click', THD.Comment.createEditForm);
+      });
+    },
     
-    var toggleLink = new Element('a', { href:"#", 'class':'showToggle'});
-    toggleLink.insert("Hide Comment");
-    toggleLink.observe('click', toggleComment)
-    controls.insert({top: toggleLink});
+    toggleComment : function(e) {
+      var that = this;
+      that.stopObserving('click');
+      that.observe('click', function(e){e.stop();})
+      var commentBody = that.up(".comment").down(".body");
+      var reattachEvent = function(){
+        that.stopObserving('click');
+        that.observe('click', THD.Comment.toggleComment);
+      }
+      if(commentBody.visible()){
+          Effect.SlideUp(commentBody, {afterFinish : reattachEvent });
+          that.update("Show Comment");
+      }else{
+          Effect.SlideDown(commentBody, {afterFinish : reattachEvent });
+          that.update("Hide Comment");
+      }
+      e.stop();
+    },
     
-    var editLink = comment.getElementsByClassName('edit-link')[0];
-    editLink.observe('click', createEditForm);
-  });
-}
-var toggleComment = function(e) {
-  var that = this;
-  that.stopObserving('click');
-  that.observe('click', function(e){e.stop();})
-  var commentBody = that.up(".comment").down(".body");
-  var reattachEvent = function(){
-    that.stopObserving('click');
-    that.observe('click', toggleComment);
-  }
-  if(commentBody.visible()){
-      Effect.SlideUp(commentBody, {afterFinish : reattachEvent });
-      that.update("Show Comment");
-  }else{
-      Effect.SlideDown(commentBody, {afterFinish : reattachEvent });
-      that.update("Hide Comment");
-  }
-  e.stop();
-}
-
-var createEditForm = function(e){
-  var commentBody = this.up('.comment').down('.body').down('.content');
-  commentBody.oldHTML = commentBody.innerHTML;
-  commentBody.update(busyImageTag);
-  new Ajax.Request( this.href, {evalScripts : true, method : 'get' });
-  this.update("Cancel");
-  this.stopObserving('click');
-  this.observe('click', cancelCommentEdit);
-  e.stop();
-}
-
-var cancelCommentEdit = function(e) {
-  var commentBody = this.up('.comment').down('.body').down('.content');
-  var commentID = commentBody.id.gsub("comment_body_", '');
-  //commentBody.update(busyImageTag);
-  //new Ajax.Updater(commentBody, "/comments/"+commentID+"/edit?cmd=cancel", {evalScripts : true, method : 'get' });
-  commentBody.update(commentBody.oldHTML);
-  var editLink = commentBody.up('.comment').down('.edit-link');
-  editLink.update("Edit");
-  editLink.stopObserving('click');
-  editLink.observe('click', createEditForm)
-  Effect.ScrollTo(commentBody.up('.comment'));
-  e.stop();
-}
-
-var submitCommentChanges = function(e) {
-  var commentBody = this.up('.comment').down('.body').down('.content');
-  commentBody.update(busyImageTag);
-  new Ajax.Updater( commentBody, this.action, {
-    parameters : this.serialize(),
-    onComplete : function() {
-        Effect.ScrollTo(commentBody.up('.comment'));
+    createEditForm : function(e){
+      var commentBody = this.up('.comment').down('.body').down('.content');
+      commentBody.oldHTML = commentBody.innerHTML;
+      commentBody.update(THD.busyImageTag);
+      new Ajax.Request( this.href, {evalScripts : true, method : 'get' });
+      this.update("Cancel");
+      this.stopObserving('click');
+      this.observe('click', THD.Comment.cancelCommentEdit);
+      e.stop();
+    },
+    
+    cancelCommentEdit : function(e) {
+      var commentBody = this.up('.comment').down('.body').down('.content');
+      commentBody.update(commentBody.oldHTML);
+      THD.Comment.resetEditButton(
+        commentBody.up('.comment').down('.edit-link')
+      );
+      Effect.ScrollTo(commentBody.up('.comment'));
+      e.stop();
+    },
+    
+    submitCommentChanges : function(e) {
+      var form = e.target;
+      var commentBody = form.up('.comment').down('.body').down('.content');
+      commentBody.update(THD.busyImageTag);
+      new Ajax.Updater( commentBody, form.action, {
+        parameters : form.serialize(),
+        onComplete : function() {
+            Effect.ScrollTo(commentBody.up('.comment'));
+        }
+      });
+      THD.Comment.resetEditButton(
+        commentBody.up('.comment').down('.edit-link')
+      );
+      e.stop();
+    },
+    
+    resetEditButton : function(button) {
+      button.update("Edit").stopObserving('click').observe(
+        'click', THD.Comment.createEditForm
+      );
     }
-  });
-  e.stop();
-  var editLink = commentBody.up('.comment').down('.edit-link');
-  editLink.update("Edit");
-  editLink.stopObserving('click');
-  editLink.observe('click', createEditForm)
-}
+  }
+});
 
-busyImage = new Image();
-busyImage.src = "/images/busy.gif";
-busyImageTag = new Element('div', {align:'center'}).update(
-  new Element('img', {src : busyImage.src})
-);
+THD.busyImageTag = function(){
+  return new Element('div', {align:'center'}).update(
+    new Element('img', {src : THD.busyImage.src})
+  );
+}();
 
-Event.observe(document, 'dom:loaded', loadCommentControls)
+Event.observe(document, 'dom:loaded', THD.Comment.loadCommentControls)
